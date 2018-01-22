28351f03a432d7e51c86557e8b0191fe532c6876
loriick <loricktravailleur@gmail.com>
2018-01-22 13:38:59 +0100

    maj

--- js/script.js
+++ js/script.js
@@ -1,28 +1,24 @@
 var title = document.querySelector('.player-title');
 var audioPlayer = document.querySelector('.player-audio');
-var pochette = document.querySelector('.pochette-pochetteImg');
+var pochette = document.querySelector('.player-pochetteImg');
+var playPause = document.querySelector('.player-audioPlayPause');
+
+
 
 //generer mes titres a partir de mon data
 for (let i = 0; i < audio.length; i++) {
-    title.innerHTML += '<p class="titleItem">' + audio[i].artist + " " + audio[i].title + '</p>';
+    title.innerHTML += '<p class="player-titleArtist">' + audio[i].artist + " " + '<span class="player-title">' + audio[i].title + '</span>' + '</p>';
 }
 
+var titleItem = document.querySelectorAll('.player-titleArtist');
+//assigner les attributs au clic du titre
 
-//assigner l'attribut au clic du titre
-var titleItem = document.querySelectorAll('.titleItem');
 for (let j = 0; j < titleItem.length; j++) {
     titleItem[j].addEventListener('click', () => {
-        for (let i = 0; i < audio.length; i++) {
-            audioPlayer.setAttribute('src', audio[j].audioSrc)
-        }
+        pochette.setAttribute('src', audio[j].imgSrc)
+        audioPlayer.setAttribute('src', audio[j].audioSrc);
+        audioPlayer.play();
     })
 }
 
-//assigner les pochette d'albums
-for (let j = 0; j < titleItem.length; j++) {
-    titleItem[j].addEventListener('click', () => {
-        for (let i = 0; i < audio.length; i++) {
-            pochette.setAttribute('src', audio[j].imgSrc)
-        }
-    })
-}
+
