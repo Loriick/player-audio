b25ed0f7024719606098a883e488d1a239685f32
loriick <loricktravailleur@gmail.com>
2018-01-22 15:05:35 +0100

    nouvelle fonctionnalité

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 var title = document.querySelector('.player-title');
 var audioPlayer = document.querySelector('.player-audio');
 var pochette = document.querySelector('.player-pochetteImg');
+var play = document.querySelector('.player-playPause');
 
+const playPause = () => {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        play.setAttribute('src', 'img/pause.png');
+    } else {
+        audioPlayer.pause();
+        play.setAttribute('src', 'img/play.png')
+    }
+};
 
 
 //generer mes titres a partir de mon data
 for (let i = 0; i < audio.length; i++) {
-  title.innerHTML += '<p class="player-titleArtist">' + audio[i].artist + " " + '<span class="player-title">' + audio[i].title + '</span>' + '</p>';
+
+
+    title.innerHTML += '<div class="player-titleContainer"> <img class="player-miniature" src="' + audio[i].imgSrc + '"/> <p class="player-titleArtist">' + audio[i].artist + "<br/>" + '<span class="player-titleTitle">' + audio[i].title + '</span> </p> </div>';
 }
 
 var titleItem = document.querySelectorAll('.player-titleArtist');
 //assigner les attributs au clic du titre
 
 for (let j = 0; j < titleItem.length; j++) {
-  titleItem[j].addEventListener('click', () => {
-    pochette.setAttribute('src', audio[j].imgSrc)
-    audioPlayer.setAttribute('src', audio[j].audioSrc);
-    audioPlayer.play();
-  })
+    titleItem[j].addEventListener('click', () => {
+        pochette.setAttribute('src', audio[j].imgSrc)
+        audioPlayer.setAttribute('src', audio[j].audioSrc);
+        playPause();
+    })
 }
 
-var play = document.querySelector('.player-play');
-var pause = document.querySelector('.player-pause');
+//boutton play/pause
+play.addEventListener('click', playPause, false);
 
-play.addEventListener('click', function() {
-  audioPlayer.play();
-  console.log('heel');
-})
 
-pause.addEventListener('click', function() {
-  audioPlayer.pause();
-  console.log('heel');
-})
