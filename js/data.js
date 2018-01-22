bb52dfcefec3f5369e5cbe75f8958eb59a55920b
loriick <loricktravailleur@gmail.com>
2018-01-22 11:28:06 +0100

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,41 @@
+var audio = [{
+    artist: 'Ty dolla $ign',
+    title: 'Droptop in the rain feat Torey Lanez',
+    audioSrc: 'audio/Ty Dolla $ign - Droptop In The Rain feat.  Torey Lanez [Official Audio].mp3',
+    imgSrc: 'img/ty.jpg'
+},
+{
+    artist: 'Andy Mineo',
+    title: 'Dunk Content(Magic Bird) feat Wordsplayed',
+    audioSrc: 'audio/Andy Mineo & Wordsplayed - DUNK CONTEST (MAGIC BIRD) [Audio].mp3',
+    imgSrc: 'img/andy.jpg'
+},
+{
+    artist: 'Dame D.O.L.L.A.',
+    title: 'Run It Up feat Lil Wayne',
+    audioSrc: 'audio/Dame D.O.L.L.A. - Run It Up (Ft. Lil Wayne).mp3',
+    imgSrc: 'img/run.jpg'
+},
+{
+    artist: 'Dj Khaled',
+    title: 'Gold Slugs feat Chris Brown, August, Alsina and Fetty Wap',
+    audioSrc: 'audio/dj-khaled-gold-slugs-audio-ft-chris-brown-august-alsina-fetty-wap.mp3',
+    imgSrc: 'img/gold.jpg'
+
+
+},
+{
+    artist: 'Miguel',
+    title: 'Banana Clip',
+    audioSrc: 'audio/Miguel - Banana Clip (Audio).mp3',
+    imgSrc: 'img/bf.jpg'
+
+
+},
+{
+    artist: 'Post Malone',
+    title: 'Rockstar feat 21 Savage',
+    audioSrc: 'audio/post-malone-rockstar-ft-21-savage-official-audio.mp3',
+    imgSrc: 'img/rockstar.jpg'
+}]
+
