a701a3c66bb6b732a9e95e2f25c20a245ea0259d
loriick <loricktravailleur@gmail.com>
2018-01-22 16:02:12 +0100

    dernière fonction

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 var title = document.querySelector('.player-title');
 var audioPlayer = document.querySelector('.player-audio');
 var pochette = document.querySelector('.player-pochetteImg');
-var play = document.querySelector('.player-playPause');
+var playBtn = document.querySelector('.player-playPause');
+var previousBtn = document.querySelector('.player-previous');
+var nextBtn = document.querySelector('.player-next');
+var currentTrack = 0;
+
+
 
 const playPause = () => {
     if (audioPlayer.paused) {
         audioPlayer.play();
-        play.setAttribute('src', 'img/pause.png');
+        playBtn.setAttribute('src', 'img/pause.png');
     } else {
         audioPlayer.pause();
-        play.setAttribute('src', 'img/play.png')
+        playBtn.setAttribute('src', 'img/play.png')
     }
 };
 
 
 //generer mes titres a partir de mon data
 for (let i = 0; i < audio.length; i++) {
-
-
-    title.innerHTML += '<div class="player-titleContainer"> <img class="player-miniature" src="' + audio[i].imgSrc + '"/> <p class="player-titleArtist">' + audio[i].artist + "<br/>" + '<span class="player-titleTitle">' + audio[i].title + '</span> </p> </div>';
+    title.innerHTML += '<div class="player-titleContainer"> <img class="player-miniature" src="' +
+        audio[i].imgSrc + '"/> <p class="player-titleArtist">' + audio[i].artist + "<br/>" +
+        '<span class="player-titleTitle">' + audio[i].title + '</span> </p> </div>';
 }
 
 var titleItem = document.querySelectorAll('.player-titleArtist');
@@ -28,11 +33,36 @@ for (let j = 0; j < titleItem.length; j++) {
     titleItem[j].addEventListener('click', () => {
         pochette.setAttribute('src', audio[j].imgSrc)
         audioPlayer.setAttribute('src', audio[j].audioSrc);
+        currentTrack = j;
+
         playPause();
     })
 }
 
 //boutton play/pause
-play.addEventListener('click', playPause, false);
+playBtn.addEventListener('click', playPause, false);
+
+//previous btn
+previousBtn.addEventListener('click', () => {
+    if (currentTrack > 0) {
+        currentTrack -= 1
+        console.log(currentTrack);
+        pochette.setAttribute('src', audio[currentTrack].imgSrc)
+        audioPlayer.setAttribute('src', audio[currentTrack].audioSrc);
+        playPause();
+    }
+});
+
+//next btn 
+nextBtn.addEventListener('click', () => {
+    if (currentTrack < audio.length - 1) {
+        currentTrack += 1
+        console.log(currentTrack);
+        pochette.setAttribute('src', audio[currentTrack].imgSrc)
+        audioPlayer.setAttribute('src', audio[currentTrack].audioSrc);
+        playPause();
+    }
+});
+
 
 
